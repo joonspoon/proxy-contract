9f20886f1bda51d239728e9d68fcbe0a7e7fedf4
joonspoon <joonspoon@joonspoon.com>
2021-12-13 22:01:28 -0800

    Added rudimentary fee collection & withdrawal.

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 
@@ -15,20 +16,21 @@ contract Swap is OwnableUpgradeable {
       address _tokenOut,
       uint _amountIn,
       uint _amountOutMin,
-      address _to
+      address _to ///TODO: Remove since sender will always be the recepient
     ) external {
 
     TransferHelper.safeTransferFrom(_tokenIn, msg.sender, address(this), _amountIn);
     TransferHelper.safeApprove(_tokenIn, UNISWAP_V2_ROUTER, _amountIn);
-    // IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-    // IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
+
+    uint feePercentage = 1;  // charge a 1% swap fee
+    uint edgeFee = SafeMathUpgradeable.div(SafeMathUpgradeable.mul(_amountIn, feePercentage), 100);
 
     address[] memory path = new address[](2);
     path[0] = _tokenIn;
     path[1] = _tokenOut;
 
     IUniswapV2Router02(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
-      _amountIn,
+      _amountIn - edgeFee,
       _amountOutMin,
       path,
       _to,
@@ -36,18 +38,11 @@ contract Swap is OwnableUpgradeable {
     );
   }
 
-  // function getAmountOutMin(
-  //   address _tokenIn,
-  //   address _tokenOut,
-  //   uint _amountIn
-  // ) external view returns (uint) {
-  //
-  //   address[] memory path = new address[](2);
-  //   path[0] = _tokenIn;
-  //   path[1] = _tokenOut;
-  //
-  //   uint[] memory amountOutMins = IUniswapV2Router02(UNISWAP_V2_ROUTER).getAmountsOut(_amountIn, path);
-  //
-  //   return amountOutMins[path.length - 1];
-  // }
+  //TODO: restrict to owner
+  function withdrawFees(address _token, address _recipient) external {
+      ERC20Upgradeable tokenRequested = ERC20Upgradeable(_token);
+      TransferHelper.safeApprove(_token, address(this), tokenRequested.balanceOf(address(this)));
+      TransferHelper.safeTransferFrom(_token, address(this), _recipient, tokenRequested.balanceOf(address(this)));
+    }
+
 }
