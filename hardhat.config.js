c1e341239999acd02c5fffe953b598bc62e87a02
joonspoon <joonspoon@joonspoon.com>
2022-05-16 19:13:01 -0700

    Switched to using private key rather than mnemonic for contract deployment.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,19 @@
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
 
-const { alchemyApiKey, seedPhrase } = require('./secrets.json');
+const { alchemyApiKey, privateKey } = require('./secrets.json');
 
 module.exports = {
    solidity: "0.8.4",
 
    networks: {
+     goerli: {
+       url: `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
+       accounts: [ privateKey ]
+     },
      rinkeby: {
        url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
-       accounts: { mnemonic: seedPhrase },
+       accounts: [ privateKey ]
      },
    },
 };
